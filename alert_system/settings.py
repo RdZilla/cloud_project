efcccd4f29ff04dbb5e0e8fc1604463655ac972e
Pavel <pavel.eliseev.work@yandex.ru>
2024-11-17 03:38:31 +0300

    fix CSRF token

--- alert_system/settings.py
+++ alert_system/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(' ')
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'http://localhost').split(' ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
